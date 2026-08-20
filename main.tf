f0c751867e6316385178915ae99ce0e31832fb02
Operative88 <wojciechkocemba21@gmail.com>
2026-08-20 17:45:41 +0200

    fix syntax at 27 line

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+# Wgraj klucz publiczny do DigitalOcean
+resource "digitalocean_ssh_key" "default" {
+  name       = var.ssh_key_name
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+# Utwórz Droplet
+resource "digitalocean_droplet" "web" {
+  image    = "ubuntu-24-04-x64"
+  name     = "terraform-droplet"
+  region   = "fra1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
+
   provisioner "remote-exec" {
     inline = ["echo 'Droplet gotowy!'"]
 
@@ -9,3 +23,4 @@
       timeout     = "2m"
     }
   }
+}
